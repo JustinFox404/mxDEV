ff2b333b1e8d2f4a38a9614a18435052ce4aa2cb
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2018-09-28 21:35:51 +0200

    Header responsive update
    + hover effect

--- protected/header.php
+++ protected/header.php
@@ -4,12 +4,14 @@
     <title>Tutorial</title>
     <link rel="stylesheet" type="text/css" href="../css/style.css">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <header>
     <div class="main"><img src="../img/logo_standalone.svg">
+        <input id="hamburger" type="checkbox"><label for="hamburger"><i id="hamopen" class="fas fa-bars"></i></label>
         <div id="nav">
             <a href="#">Info</a><a href="#">About</a><a href="#">Contact</a>
         </div>
