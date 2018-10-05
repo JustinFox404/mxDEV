923ea44be810cc20953fc0f03b200ba058b0efa8
HHK3 <joeltilon@live.nl>
2018-10-05 12:37:21 +0200

    Fixed the Mail Function with PHPMailer (Master Version)

--- functions/mail.php
+++ functions/mail.php
@@ -2,7 +2,7 @@
 
 // Should the person be here?
 if (!isset($_POST['submit_contact'])) {
-    header('Location: ../../page/index.php?page=contact');
+    header('Location: ../page/index.php?page=contact');
 }
 
 // Defining the variables
@@ -12,30 +12,52 @@ if (!isset($_POST['submit_contact'])) {
     $subject = $_POST['onderwerp'];
     $text = htmlentities($_POST['tekst']);
 
-    // To the receiver
-    $to = "mxdev.nl@gmail.com";
-
-    // Headers
-    $headers = 'From: ' . $mailaddress . "\r\n";
-    $headers .= 'Reply-To: ' . $mailaddress . "\r\n";
-    $headers .= "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
     // Subject (real one)
     $subject1 = 'Nieuw bericht vanuit het Contact Formulier: ' . $subject;
 
-    // Message
-    $message = "
-    <h1>Hallo MxDev team!</h1> 
-    
-    <p>Jullie hebben een nieuw mail ontvangen vanuit het Contact Formulier.</p>
-    <p><b>Naam:</b> "  . $name . "</p>
-    <p><b>E-mail:</b> " . $mailaddress . "</p>
-    <p><b>Onderwerp:</b> " . $subject . "</p>
-    <p><b>Bericht:</b>
-    <p>" . $text . "</p>" or die('Error 6');
+//Setting up PHPMailer
+    // Including PHPMailerAutoload.php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
-mail($to, $subject1, $message, $headers) or die ('Error mailing');
+    require '../phpmailer/src/Exception.php';
+    require '../phpmailer/src/PHPMailer.php';
+    require '../phpmailer/src/SMTP.php';
 
-header('Location: ../page/index.php?page=gelukt_mail');
-exit();
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'mxdev.nl@gmail.com';
+        $mail->Password = 'E2+bCr+M$Z&Yp&E+';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        //Recipients
+        $mail->setFrom("mxdev.nl@gmail.com", "mxDEV");
+        $mail->addAddress('mxdev.nl@gmail.com', 'MxDev');
+        $mail->addReplyTo($mailaddress, $name);
+
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = $subject1;
+        $mail->Body    = "
+        <h1>Hallo MxDev team!</h1>
+        
+        <p>Jullie hebben een nieuw mail ontvangen vanuit het Contact Formulier.</p>
+        <p><b>Naam:</b> "  . $name . "</p>
+        <p><b>E-mail:</b> " . $mailaddress . "</p>
+        <p><b>Onderwerp:</b> " . $subject . "</p>
+        <p><b>Bericht:</b>
+        <p>" . $text . "</p>";
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->send();
+        header('Location: ../page/index.php?page=gelukt_mail');
+    } catch (Exception $e) {
+        header('Location: ../page/index.php?page=contact&gelukt=error');
+    }
+
+?>
